8eadfb1d5d9ce4c1390e2655db8a221f305e2136
Krishna Sivakumar <krishnasivaprogrammer@gmail.com>
2020-12-24 22:24:02 +0530

    hello world and simple programs work on this

--- bf.py
+++ bf.py
@@ -3,25 +3,41 @@ import argparse
 array = [0 for i in range(30000)]
 curr_pointer = 0
 
-with open("input.bf", 'r') as f:
-    for command in f.read():
-        if command not in '<>+-.,[]':
-            print(f'"{command}" is not a recognized command')
-            exit()
+file = open("input.bf", 'r').read()
+file = "".join([x for x in file if x in '<>+-.,[]'])
 
-        if command == '>':
-            curr_pointer = (curr_pointer + 1) % 30000
-        elif command == '<':
-            curr_pointer = (curr_pointer - 1) % 30000
-        elif command == '+':
-            array[curr_pointer] += 1
-        elif command == '-':
-            array[curr_pointer] -= 1
-        elif command == '.':
-            print(array[curr_pointer], end='')
-        elif command == ',':
-            array[curr_pointer] = int(input())
-        elif command == '[':
-            pass
-        elif command == ']':
-            pass
+curr_command, max_len = 0, len(file)
+loop_stack, skip_loop = [], False
+
+while curr_command < max_len:
+    command = file[curr_command]
+
+    if skip_loop:
+        skip_loop = file[curr_command] != ']'
+        curr_command += 1
+        continue
+
+    if command == '>':
+        curr_pointer = curr_pointer + 1
+    elif command == '<':
+        curr_pointer = curr_pointer - 1
+    elif command == '+':
+        array[curr_pointer] += 1
+    elif command == '-':
+        array[curr_pointer] -= 1
+    elif command == '.':
+        print(chr(array[curr_pointer]), end='')
+    elif command == ',':
+        array[curr_pointer] = int(input())
+    elif command == '[':
+        skip_loop = array[curr_pointer] == 0
+        if not skip_loop:
+            loop_stack.append(curr_command)
+    elif command == ']':
+        if array[curr_pointer] != 0:
+            curr_command = loop_stack[-1] + 1
+            continue
+        else:
+            loop_stack.pop()
+
+    curr_command += 1

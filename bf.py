afd1a2eedfa63bcc56fe8b11005e0dcef669cf9a
Krishna Sivakumar <krishnasivaprogrammer@gmail.com>
2020-12-25 00:29:52 +0530

    more readable code

--- bf.py
+++ bf.py
@@ -1,43 +1,42 @@
 import argparse
 
-array = [0 for i in range(30000)]
-curr_pointer = 0
+array = [0 for i in range(30)]
+data_pointer = 0
 
 file = open("input.bf", 'r').read()
-file = "".join([x for x in file if x in '<>+-.,[]'])
+program = "".join([x for x in file if x in '<>+-.,[]'])
 
-curr_command, max_len = 0, len(file)
-loop_stack, skip_loop = [], False
+loops = []
 
-while curr_command < max_len:
-    command = file[curr_command]
+instruction = 0
 
-    if skip_loop:
-        skip_loop = file[curr_command] != ']'
-        curr_command += 1
-        continue
+while instruction < len(program):
+    command = program[instruction]
 
     if command == '>':
-        curr_pointer = curr_pointer + 1
+        data_pointer += 1
     elif command == '<':
-        curr_pointer = curr_pointer - 1
+        data_pointer -= 1
     elif command == '+':
-        array[curr_pointer] += 1
+        array[data_pointer] += 1
     elif command == '-':
-        array[curr_pointer] -= 1
+        array[data_pointer] -= 1
     elif command == '.':
-        print(chr(array[curr_pointer]), end='')
+        print(chr(array[data_pointer]), end='')
     elif command == ',':
-        array[curr_pointer] = int(input())
+        inp = input()
+        array[data_pointer] = int(inp) if not inp.isalpha() else ord(inp)
+        pass
     elif command == '[':
-        skip_loop = array[curr_pointer] == 0
-        if not skip_loop:
-            loop_stack.append(curr_command)
-    elif command == ']':
-        if array[curr_pointer] != 0:
-            curr_command = loop_stack[-1] + 1
-            continue
+        if array[data_pointer] == 0:
+            while program[instruction] != ']':
+                instruction += 1
         else:
-            loop_stack.pop()
+            loops.append(instruction)
+    elif command == ']':
+        if array[data_pointer] != 0:
+            instruction = loops[-1]
+        else:
+            loops.pop()
 
-    curr_command += 1
+    instruction += 1

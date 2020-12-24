c5acad5ae708ef3951be20401b40ef9dfc02ffe7
Krishna Sivakumar <krishnasivaprogrammer@gmail.com>
2020-12-24 12:54:07 +0530

    added non-loop commands

--- bf.py
+++ bf.py
@@ -1,12 +1,27 @@
 import argparse
 
-commands = set(['<', '>', '+', '-', '.', ',', '[', ']'])
-
 array = [0 for i in range(30000)]
 curr_pointer = 0
 
-with f as open("input.bf", 'r'):
+with open("input.bf", 'r') as f:
     for command in f.read():
-        if command not in commands:
+        if command not in '<>+-.,[]':
             print(f'"{command}" is not a recognized command')
             exit()
+
+        if command == '>':
+            curr_pointer = (curr_pointer + 1) % 30000
+        elif command == '<':
+            curr_pointer = (curr_pointer - 1) % 30000
+        elif command == '+':
+            array[curr_pointer] += 1
+        elif command == '-':
+            array[curr_pointer] -= 1
+        elif command == '.':
+            print(array[curr_pointer], end='')
+        elif command == ',':
+            array[curr_pointer] = int(input())
+        elif command == '[':
+            pass
+        elif command == ']':
+            pass
